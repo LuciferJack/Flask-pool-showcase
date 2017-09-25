4325ecb8546557211af657bfaf25dc8842a982ed
lujunxu <lujunxu@baidu.com>
2017-09-25 20:56:33 +0800

    add prototype

---
+++ FlaskPoolShowcase/mysql_config.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# coding=utf-8
+"""
+can more db ,more pool
+"""
+db_config = {
+    'local': {
+        'host': "10.95.130.***", 'port': 8899,
+        'user': "root", 'passwd': "******",
+        'db': "marry", 'charset': "utf8",
+        'pool': {
+            #use = 0 no pool else use pool
+            "use":1,
+            # size is >=0,  0 is dynamic pool
+            "size":10,
+            #pool name
+            "name":"local",
+        }
+    },
+    'poi': {
+        'host': "10.95.130.***", 'port': 8787,
+        'user': "lujunxu", 'passwd': "****",
+        'db': "poi_relation", 'charset': "utf8",
+        'pool': {
+            #use = 0 no pool else use pool
+            "use":0,
+            # size is >=0,  0 is dynamic pool
+            "size":0,
+            #pool name
+            "name":"poi",
+        }
+    },
+}
--- show_case.py
+++ show_case.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #coding=utf-8
-from PyMysqlPool.db_util.db_config.mysql_config import db_config
-from PyMysqlPool.db_util.mysql_util import query
+import logging
+from PyMysqlPool.db_util.mysql_util import query, insertOrUpdate
+
+from FlaskPoolShowcase.mysql_config import db_config
 
 
-def flask_quer():
+def flask_query():
     job_status = 2
     _sql = "select *  from master_job_list j  where j.job_status  !=%s "
     _args = (job_status,)
     task = query(db_config['local'], _sql,_args)
     logging.info("query_npool method query_npool result is %s ,input _data is %s ", task , _args)
 
+def flask_insertOrUpdate(nlp_rank_id,hit_query_word):
+    #add more args
+    _args = (nlp_rank_id,hit_query_word)
+    _sql = """INSERT INTO nlp_rank_poi_online (nlp_rank_id,hit_query_word,rank_type,poi_list,poi_raw_list,article_id,city_id,status,create_time,version,source_from) VALUES (%s,%s,%s, %s, %s,%s, %s,%s, %s,%s,%s)"""
+    affect = insertOrUpdate(db_config['local'], _sql, _args)
+    logging.info("insert method insert result is %s ,input _data is %s ", affect , _args)
+
+
 if __name__ == "__main__":
+    flask_query()
